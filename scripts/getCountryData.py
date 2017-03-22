b7342a03e829b91c54a033014c92f8dff679b21a
allisonking <allisonjuliaking@gmail.com>
2017-03-21 20:29:04 -0400

    script for getting country data

---
+++ scripts/getCountryData.py
@@ -0,0 +1,20 @@
+"""Script to get the country that a movie is from and add it to the CSV file"""
+
+import sys
+import requests
+
+def main():
+    """Main entry point for the script."""
+
+    pass
+
+def get_array_of_countries(url):
+    """Returns a list of countries associated with a given movie"""
+    pass
+
+def append_to_csv(countries):
+    """Appends the countries to the csv file"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main());
