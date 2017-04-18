99884323d505923041a49f475450a95312f8f7da
allisonking <allisonjuliaking@gmail.com>
2017-04-17 21:26:04 -0400

    compares popularity, item similarity, and ranking factorization algorithms

--- recommenders/compare_recommenders.py
+++ recommenders/compare_recommenders.py
@@ -5,15 +5,30 @@ import graphlab as gl
 
 
 def main():
+    # get our data sets
+    ratings_path = '../movie-lens-data/ratings.csv'
+    train, test = get_training_test_data(ratings_path)
+
     # load in our competing models
-    popularity_model = gl.load_model('saved-models/popularity_model')
-    item_similarity_model = gl.load_model('saved-models/item_similarity_model')
-    ranking_factorization_model = gl.load_model('saved-models/ranking_factorization_model')
+    popularity_model = gl.popularity_recommender.create(train, user_id='userId', item_id='movieId', target='rating')
+    item_similarity_model = gl.item_similarity_recommender.create(train, user_id='userId', item_id='movieId',
+                                                                  target='rating', similarity_type='cosine')
+    ranking_factorization_model = gl.ranking_factorization_recommender.create(train, user_id='userId', item_id='movieId',
+                                                                              target='rating')
 
-    # get some data to test on. TODO: read in uniform training/test data in separate file
-    test_data = gl.SFrame.read_csv('../movie-lens-data/ratings.csv')
+    print "==== Comparing Models ===="
+    gl.recommender.util.compare_models(test, [popularity_model, item_similarity_model, ranking_factorization_model],
+                                       metric='precision_recall')
 
 
+def get_training_test_data(path):
+    # read in data
+    ratings_file = '../movie-lens-data/ratings.csv'
+    ratings_data = gl.SFrame.read_csv(ratings_file)
+
+    train, test = gl.recommender.util.random_split_by_user(ratings_data, 'userId', 'movieId')
+
+    return train, test
 
 if __name__ == '__main__':
     sys.exit(main())
